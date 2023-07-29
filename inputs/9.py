1bf0718be878108c27e32ee99c31fbde5d782add
Chris Truter <chris.truter.za@gmail.com>
2023-07-29 21:43:33 +0200

    update python

--- inputs/9.py
+++ inputs/9.py
@@ -1,20 +1,25 @@
-rope = [0] * 10
-seen = [{x} for x in rope]
-dirs = {'L': +1, 'R': -1, 'D': 1j, 'U': -1j}
-
-
 def sign(x):
     return complex((x.real > 0) - (x.real < 0), (x.imag > 0) - (x.imag < 0))
 
 
-for line in open('09.example2.txt'):
-    for _ in range(int(line[2:])):
-        rope[0] += dirs[line[0]]
+def solve(path):
+    rope = [0] * 10
+    seen = [{x} for x in rope]
+    dirs = {'L': +1, 'R': -1, 'D': 1j, 'U': -1j}
 
-        for i in range(1, 10):
-            dist = rope[i - 1] - rope[i]
-            if abs(dist) >= 2:
-                rope[i] += sign(dist)
-                seen[i].add(rope[i])
+    for line in open(path):
+        for _ in range(int(line[2:])):
+            rope[0] += dirs[line[0]]
 
-print(len(seen[1]), len(seen[9]))
+            for i in range(1, 10):
+                dist = rope[i - 1] - rope[i]
+                if abs(dist) >= 2:
+                    rope[i] += sign(dist)
+                    seen[i].add(rope[i])
+
+    print(len(seen[1]), len(seen[9]))
+
+
+solve('09.example.txt')
+solve('09.example2.txt')
+solve('09.txt')
